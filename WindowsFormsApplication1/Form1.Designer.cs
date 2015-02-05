979f3d17067c9a08597e573106edea3fbd2d9c7b
Snikaz <bentdal92@gmail.com>
2015-02-05 12:51:48 +0100

    Some problem with labels.

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -67,7 +67,7 @@
             this.comboBox1.Location = new System.Drawing.Point(12, 190);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 4;
+            this.comboBox1.TabIndex = 5;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
@@ -76,7 +76,7 @@
             this.label1.Location = new System.Drawing.Point(12, 237);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 6;
+            this.label1.TabIndex = 7;
             this.label1.Text = "label1";
             // 
             // label2
@@ -85,7 +85,7 @@
             this.label2.Location = new System.Drawing.Point(10, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(107, 13);
-            this.label2.TabIndex = 8;
+            this.label2.TabIndex = 9;
             this.label2.Text = "Write number of days";
             // 
             // label3
@@ -94,7 +94,7 @@
             this.label3.Location = new System.Drawing.Point(9, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(239, 13);
-            this.label3.TabIndex = 7;
+            this.label3.TabIndex = 8;
             this.label3.Text = "Write zoom: default 330. Increase for better zoom";
             // 
             // textBox2
@@ -102,14 +102,14 @@
             this.textBox2.Location = new System.Drawing.Point(12, 128);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.textBox2.TabIndex = 3;
             // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(13, 154);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
+            this.button2.TabIndex = 4;
             this.button2.Text = "Update zoom";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -120,7 +120,7 @@
             this.checkBox1.Location = new System.Drawing.Point(13, 217);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(79, 17);
-            this.checkBox1.TabIndex = 5;
+            this.checkBox1.TabIndex = 6;
             this.checkBox1.Text = "ShowLabel";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
@@ -131,7 +131,7 @@
             this.label4.Location = new System.Drawing.Point(12, 360);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(150, 13);
-            this.label4.TabIndex = 9;
+            this.label4.TabIndex = 10;
             this.label4.Text = "Change speed. Default 20(ms)";
             // 
             // textBox3
@@ -169,7 +169,7 @@
             this.button4.Location = new System.Drawing.Point(12, 87);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 13;
+            this.button4.TabIndex = 2;
             this.button4.Text = "Pause/Resume";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
